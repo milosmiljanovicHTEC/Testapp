78623891f075f0d00333b45f5121a82496d6d2ac
Milos Miljanovic <milos.miljanovic@htecgroup.com>
2022-06-13 11:47:48 +0200

    added runner script

---
+++ runner.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ "$1" == "production" ] || [ "$1" == "prod" ]; then
+    docker-compose -f docker-compose.yaml -f docker-compose.production.yaml up --build --remove-orphans -d
+elif [ -z "$1" ] || [ "$1" == "development" ] || [ "$1" == "dev" ]; then
+    docker-compose -f docker-compose.yaml -f docker-compose.development.yaml up --build --remove-orphans -d
+else
+    echo "Unknown environment: $1";
+    exit 1;
+fi
+
+
+# Remove old images, containers, networks...
+docker system prune -f
