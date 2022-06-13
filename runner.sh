ea45b5defcd6a089a5c8f8d4e8338ec79868f961
Milos Miljanovic <milos.miljanovic@htecgroup.com>
2022-06-13 14:34:48 +0200

    pwd1

--- runner.sh
+++ runner.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "$1" == "production" ] || [ "$1" == "prod" ]; then pwd && ls
-    docker-compose -f docker-compose.production.yaml up --build --remove-orphans -d
+    docker-compose -f docker-compose.production.yml up --build --remove-orphans -d
 elif [ -z "$1" ] || [ "$1" == "development" ] || [ "$1" == "dev" ]; then pwd && ls
-    docker-compose -f docker-compose.development.yaml up --build --remove-orphans -d
+    docker-compose -f docker-compose.development.yml up --build --remove-orphans -d
 else
     echo "Unknown environment: $1";
     exit 1;
