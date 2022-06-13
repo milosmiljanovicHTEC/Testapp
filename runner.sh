87070616971873001cd899f78d29819dedda73de
Milos Miljanovic <milos.miljanovic@htecgroup.com>
2022-06-13 13:36:30 +0200

    fix

--- runner.sh
+++ runner.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "$1" == "production" ] || [ "$1" == "prod" ]; then
-    docker-compose -f docker-compose.yaml -f docker-compose.production.yaml up --build --remove-orphans -d
+    docker-compose -f docker-compose.production.yaml up --build --remove-orphans -d
 elif [ -z "$1" ] || [ "$1" == "development" ] || [ "$1" == "dev" ]; then
-    docker-compose -f docker-compose.yaml -f docker-compose.development.yaml up --build --remove-orphans -d
+    docker-compose -f docker-compose.development.yaml up --build --remove-orphans -d
 else
     echo "Unknown environment: $1";
     exit 1;
