249feef7663acbe1ba7ea64c46703c8a6c32fadd
milosmiljanovic <106978365+milosmiljanovic@users.noreply.github.com>
2022-06-24 16:17:54 +0200

    Update Read.sh

--- Read.sh
+++ Read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-awk -f ./tst.awk README.md > README.md
+awk -f tst.awk README.md > README.md
 awk  '{if(NR>1  && $2==$4) print "!"$0 ; else if ($2>$4) print "+"$0 ;  else if($2<$4) print "-"$0 ; else print "```diff"}' README.md > output.md
 
