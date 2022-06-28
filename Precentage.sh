632320ba8b3573b40ec3d7db07cdfbdca631dbd2
milosmiljanovic <106978365+milosmiljanovic@users.noreply.github.com>
2022-06-28 14:12:54 +0200

    Add files via upload

---
+++ Precentage.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+awk 'BEGIN { print "```diff" } {if((V=(($2 * 100)/$4)) && V < 10) print "!"$0;
+ else if ((V=(($2 * 100)/$4))&& V > 11 &&  V < 50)print "-"$0;
+  else if ((V=(($2 * 100)/$4))&& V < 100 && Q > 51 )print "+"$0;
+   else if ((V=(($2 * 100)/$4))&& V> 100){print "@@"$0, "@@"$6} else print "Undifined character" }' new.md >> new1.md
