b452a69c35fd9dd1485b8e144d75bf4ecc2cd602
Milos Miljanovic <milos.miljanovic@htecgroup.com>
2022-06-29 16:01:29 +0200

    gh3

--- Precentage.sh
+++ Precentage.sh
@@ -3,4 +3,4 @@
 awk 'NR==4, NR==9 {if((V=(($2 * 100)/$4)) && V < 10) print "!"$0;
  else if ((V=(($2 * 100)/$4))&& V > 11 &&  V < 50)print "-"$0;
   else if ((V=(($2 * 100)/$4))&& V < 100 && Q > 51 )print "+"$0;
-   else if ((V=(($2 * 100)/$4))&& V> 100){print "@@"$0"@@"} else print "Numbers are equal" }' README.md > new1.md
+   else if ((V=(($2 * 100)/$4))&& V> 100){print "@@"$0"@@"} else print "Numbers are equal" }' README.md >> new1.md
