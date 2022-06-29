4999817d35bfef3efcf532c1bb0539d8ff7b967b
milosmiljanovic <106978365+milosmiljanovic@users.noreply.github.com>
2022-06-29 10:29:46 +0200

    Update Precentage.sh

--- Precentage.sh
+++ Precentage.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-awk ' {NF>2}{if((V=(($2 * 100)/$4)) && V < 10) print "!"$0;
+awk ' {NF==3}{if((V=(($2 * 100)/$4)) && V < 10) print "!"$0;
  else if ((V=(($2 * 100)/$4))&& V > 11 &&  V < 50)print "-"$0;
   else if ((V=(($2 * 100)/$4))&& V < 100 && Q > 51 )print "+"$0;
    else if ((V=(($2 * 100)/$4))&& V > 100){print "@@"$0, "@@"$6} else print "Numbers are equal" }' new.md >> new1.md
